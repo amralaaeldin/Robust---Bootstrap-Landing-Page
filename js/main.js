64f453efe975f87f1f5b2695657b91d37dd821ad
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2023-03-28 17:32:31 +0200

    feat: added accordion section

--- js/main.js
+++ js/main.js
@@ -11,12 +11,18 @@ function applyTheme() {
     themeToggler.querySelector(".icon").classList.replace("fa-moon", "fa-sun");
     document.querySelector(".landing").classList.add("text-white");
     document.querySelector(".about").classList.add("text-white");
+    document.querySelector(".faq.accordion").classList.add("text-white");
+    document.querySelectorAll(".faq.accordion .accordion-item").forEach((item) => item.classList.add("text-white"));
+    document.querySelectorAll(".faq.accordion .accordion-header button").forEach((item) => item.classList.add("text-white"));
   } else {
     document.body.classList.remove("dark");
     document.querySelector(".logo").src = "./assets/logo.png";
     themeToggler.querySelector(".icon").classList.replace("fa-sun", "fa-moon");
     document.querySelector(".landing").classList.remove("text-white");
     document.querySelector(".about").classList.remove("text-white");
+    document.querySelector(".faq.accordion").classList.remove("text-white");
+    document.querySelectorAll(".faq.accordion .accordion-item").forEach((item) => item.classList.remove("text-white"));
+    document.querySelectorAll(".faq.accordion .accordion-header button").forEach((item) => item.classList.remove("text-white"));
   }
 }
 
