eb248ca56c0e9bfc379c59017922bcdb4421aeba
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2023-04-22 15:13:40 +0200

    feat: added footer section, fix: p global styling, change uppercase in navbar, changed corresponding js

--- js/main.js
+++ js/main.js
@@ -7,30 +7,38 @@ localStorage.getItem("dark-theme")
 function applyTheme() {
   if (parseInt(localStorage.getItem("dark-theme"))) {
     document.body.classList.add("dark");
-    document.querySelector(".logo").src = "./assets/logo-dark.png";
+    document.querySelectorAll(".logo").forEach((item) => {
+      item.src = "./assets/logo-dark.png";
+    });
     themeToggler.querySelector(".icon").classList.replace("fa-moon", "fa-sun");
     document.querySelector(".landing").classList.add("text-white");
     document.querySelector(".about").classList.add("text-white");
     document.querySelector(".faq.accordion").classList.add("text-white");
     document
-      .querySelectorAll(".faq.accordion .accordion-item")
-      .forEach((item) => item.classList.add("text-white"));
-    document
       .querySelectorAll(".faq.accordion .accordion-header button")
       .forEach((item) => item.classList.add("text-white"));
+    document
+      .querySelectorAll(".text-black-50")
+      .forEach((item) =>
+        item.classList.replace("text-black-50", "text-white-50")
+      );
   } else {
     document.body.classList.remove("dark");
-    document.querySelector(".logo").src = "./assets/logo.png";
+    document.querySelectorAll(".logo").forEach((item) => {
+      item.src = "./assets/logo.png";
+    });
     themeToggler.querySelector(".icon").classList.replace("fa-sun", "fa-moon");
     document.querySelector(".landing").classList.remove("text-white");
     document.querySelector(".about").classList.remove("text-white");
     document.querySelector(".faq.accordion").classList.remove("text-white");
     document
-      .querySelectorAll(".faq.accordion .accordion-item")
-      .forEach((item) => item.classList.remove("text-white"));
-    document
       .querySelectorAll(".faq.accordion .accordion-header button")
       .forEach((item) => item.classList.remove("text-white"));
+    document
+      .querySelectorAll(".text-white-50")
+      .forEach((item) =>
+        item.classList.replace("text-white-50", "text-black-50")
+      );
   }
 }
 
